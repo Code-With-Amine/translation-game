172ee0894db30665942bcc54913e2fb5a35d6d89
Code-With-Amine <userihj36@gmail.com>
2023-06-03 20:30:32 +0100

    I added the result and move to next answer functionalities

--- js/app.js
+++ js/app.js
@@ -12,7 +12,7 @@ $(document).ready(function(){
         const TargetedLanguagesInDatabase = ref(database, "targetedLang") 
 
 
-        const JSONwords = '{"translations":[{"sourceLanguage":"en","targetLanguage":"fr","phrases":{"hello":"bonjour","goodbye":"au revoir","thank you":"merci","cat":"chat","dog":"chien","house":"maison"}},{"sourceLanguage":"en","targetLanguage":"es","phrases":{"hello":"hola","goodbye":"adiós","thank you":"gracias","cat":"gato","dog":"perro","house":"casa"}},{"sourceLanguage":"en","targetLanguage":"ar","phrases":{"hello":"مرحبا","goodbye":"وداعا","thank you":"شكرا","cat":"قطة","dog":"كلب","house":"منزل"}},{"sourceLanguage":"fr","targetLanguage":"en","phrases":{"bonjour":"hello","au revoir":"goodbye","merci":"thank you","chat":"cat","chien":"dog","maison":"house"}},{"sourceLanguage":"fr","targetLanguage":"es","phrases":{"bonjour":"hola","au revoir":"adiós","merci":"gracias","chat":"gato","chien":"perro","maison":"casa"}},{"sourceLanguage":"fr","targetLanguage":"ar","phrases":{"bonjour":"مرحبا","au revoir":"وداعا","merci":"شكرا","chat":"قطة","chien":"كلب","maison":"منزل"}},{"sourceLanguage":"ar","targetLanguage":"en","phrases":{"مرحبا":"hello","وداعا":"goodbye","شكرا":"thank you","قطة":"cat","كلب":"dog","منزل":"house"}},{"sourceLanguage":"ar","targetLanguage":"es","phrases":{"مرحبا":"hola","وداعا":"adiós","شكرا":"gracias","قطة":"gato","كلب":"perro","منزل":"casa"}},{"sourceLanguage":"ar","targetLanguage":"fr","phrases":{"مرحبا":"bonjour","وداعا":"au revoir","شكرا":"merci","قطة":"chat","كلب":"chien","منزل":"maison"}}]}'
+        const JSONwords = '{"translations":[{"sourceLanguage":"en","targetLanguage":"fr","phrases":{"hello":"bonjour","goodbye":"au revoir","thank you":"merci","cat":"chat","dog":"chien","house":"maison","apple":"pomme","car":"voiture","book":"livre","music":"musique","friend":"ami","sun":"soleil","tree":"arbre","flower":"fleur","love":"amour","food":"nourriture"}},{"sourceLanguage":"en","targetLanguage":"es","phrases":{"hello":"hola","goodbye":"adiós","thank you":"gracias","cat":"gato","dog":"perro","house":"casa","apple":"manzana","car":"coche","book":"libro","music":"música","friend":"amigo","sun":"sol","tree":"árbol","flower":"flor","love":"amor","food":"comida"}},{"sourceLanguage":"en","targetLanguage":"ar","phrases":{"hello":"مرحبا","goodbye":"وداعا","thank you":"شكرا","cat":"قطة","dog":"كلب","house":"منزل","apple":"تفاحة","car":"سيارة","book":"كتاب","music":"موسيقى","friend":"صديق","sun":"شمس","tree":"شجرة","flower":"زهرة","love":"حب","food":"طعام"}},{"sourceLanguage":"fr","targetLanguage":"en","phrases":{"bonjour":"hello","au revoir":"goodbye","merci":"thank you","chat":"cat","chien":"dog","maison":"house","pomme":"apple","voiture":"car","livre":"book","musique":"music","ami":"friend","soleil":"sun","arbre":"tree","fleur":"flower","amour":"love","nourriture":"food"}},{"sourceLanguage":"fr","targetLanguage":"es","phrases":{"bonjour":"hola","au revoir":"adiós","merci":"gracias","chat":"gato","chien":"perro","maison":"casa","pomme":"manzana","voiture":"coche","livre":"libro","musique":"música","ami":"amigo","soleil":"sol","arbre":"árbol","fleur":"flor","amour":"amor","nourriture":"comida"}},{"sourceLanguage":"fr","targetLanguage":"ar","phrases":{"bonjour":"مرحبا","au revoir":"وداعا","merci":"شكرا","chat":"قطة","chien":"كلب","maison":"منزل","pomme":"تفاحة","voiture":"سيارة","livre":"كتاب","musique":"موسيقى","ami":"صديق","soleil":"شمس","arbre":"شجرة","fleur":"زهرة","amour":"حب","nourriture":"طعام"}},{"sourceLanguage":"ar","targetLanguage":"en","phrases":{"مرحبا":"hello","وداعا":"goodbye","شكرا":"thank you","قطة":"cat","كلب":"dog","منزل":"house","تفاحة":"apple","سيارة":"car","كتاب":"book","موسيقى":"music","صديق":"friend","شمس":"sun","شجرة":"tree","زهرة":"flower","حب":"love","طعام":"food"}},{"sourceLanguage":"ar","targetLanguage":"es","phrases":{"مرحبا":"hola","وداعا":"adiós","شكرا":"gracias","قطة":"gato","كلب":"perro","منزل":"casa","تفاحة":"manzana","سيارة":"coche","كتاب":"libro","موسيقى":"música","صديق":"amigo","شمس":"sol","شجرة":"árbol","زهرة":"flor","حب":"amor","طعام":"comida"}},{"sourceLanguage":"ar","targetLanguage":"fr","phrases":{"مرحبا":"bonjour","وداعا":"au revoir","شكرا":"merci","قطة":"chat","كلب":"chien","منزل":"maison","تفاحة":"pomme","سيارة":"voiture","كتاب":"livre","موسيقى":"musique","صديق":"ami","شمس":"soleil","شجرة":"arbre","زهرة":"fleur","حب":"amour","طعام":"nourriture"}}]}'
         const words = JSON.parse(JSONwords);
         let SourceLang = $('#SourceLang'); // retrive the select list for the source language
         let TargetLang = $('#TargetLang'); // retrive the select list for the source language
@@ -78,15 +78,21 @@ $(document).ready(function(){
 
     let phraseInTargetLang;
     let randPhrase;
-    let score = 0;
+    let rightAnswers = 0;
+    let WrongAnswers = 0;
     let attempts = 3;
-    let resultState = $('#result-state');
     let interval;
+    let resultState = $('#result-state');
+    let showrsult = $('.showResult');
+    let showHint = $('.showHint');
+    const modal = $("#myModal");
+
 
 function startTheGame(source, target){
     attempts = 3;// reinitalize the attampts
     $('.placeholder > input').val('') //clear the input
-    resultState.css('display','none')
+    showrsult.css('display','none')
+    showHint.css('display','none')
 
     $('#nextBtn').prop('disabled', true) // disable the next button
     $('#checkAnser').prop('disabled', false) // enable the check button
@@ -144,43 +150,82 @@ function CheckAnswer(){
     }
 }
 
+// this function is called whenever the answer is wrong
 function lost(reason, lostReason){
 
-    resultState.css('display','block')
+    showrsult.css('display','')
     if(attempts == 1){
         $('.placeholder > input').val('') //clear the input
-        resultState.html(`${lostReason} <span style="font-size:1.5rem; color:green;">${phraseInTargetLang}</span>` )
+        resultState.html(`${lostReason} <span style="font-size:1.5rem; color:green;">${phraseInTargetLang}</span> ` )
         clearInterval(interval);
+        showHint.fadeOut(5000)
         $('#nextBtn').prop('disabled', false)
         $('#checkAnser').prop('disabled', true)// disable the check button
-        score--;
+        WrongAnswers++;
 
     }else{
             attempts--
-            resultState.fadeIn('fast')
+            showrsult.fadeIn('fast')
             resultState.text(`${reason} you have ${attempts} attempts left`)
 
 
             setTimeout(function(){
-                resultState.fadeOut(6000);
+                showrsult.fadeOut(4000);
+                showHint.css('display','none')
                 SetGameTime()// set another timeLine for the game
 
-            },6000) // after 6 secondes hide the result state message
+            },5000) // after 6 secondes hide the result state message
     }
+    showHint.css('display','')
+    const similarityPercentage = calculateSimilarityPercentage($('.placeholder > input').val().toLowerCase(), phraseInTargetLang)
+    $('.showHint > p').text(`You were ${similarityPercentage}% close to the phrase`)
 }
 
 
 function correctAnswer(){
     $('.placeholder > input').val('') //clear the input
-    resultState.css('display','block')
-    resultState.text('Well done your translation to the phrase is correct')
+    showrsult.css('display','')
+    resultState.html('<span style="color:green;font-size:1.5rem">Well done your translation to the phrase is correct</span> <img src="images/party-popper.png" class="icon">')
             clearInterval(interval);
         $('#nextBtn').prop('disabled', false)
         $('#checkAnser').prop('disabled', true)// disable the check button
-        score++
+        rightAnswers++
 }
+
+
+function calculateSimilarityPercentage(targetWord, userInput) {
+    var maxLength = Math.max(targetWord.length, userInput.length);
+    var correctCharacters = 0;
+  
+    for (var i = 0; i < targetWord.length; i++) {
+      if (targetWord[i] === userInput[i]) {
+        correctCharacters++;
+      }
+    }
+  
+    var similarity = (correctCharacters / maxLength) * 100;
+    return similarity.toFixed(2); // Round to 2 decimal places
+  }
 
 // Go to the next word when the user clicks on the next button
 $('#nextBtn').off('click').on('click', () => startTheGame(userSourceLang, userTargetLang));
+
+// show the user score
+$('#result').on('click',function(){
+    modal.show(); // open the modal dialog
+    $('.modal-content > p').text(`You answered ${rightAnswers} out of ${rightAnswers + WrongAnswers}`)
+})
+
+// Close the modal dialog when clicked outside
+$(window).on("click", function (event) {
+    if (event.target == modal[0]) {
+      closeModal();
+    }
+  });
+
+  // Close the modal dialog
+  function closeModal() {
+    modal.hide();
+  }
 
 }) 
